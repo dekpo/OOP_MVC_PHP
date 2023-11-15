39af4d7ca049f9d48525e80d2715e5078f6c2119
Dekpo Yologaza <dekpo@me.com>
2023-11-15 12:01:40 +0100

    Created my wonderfull User class based on abstract Table

--- class/App/Models/User.php
+++ class/App/Models/User.php
@@ -9,5 +9,35 @@ class User extends Table{
     private ?string $password = null;
     private ?array $roles = [];
 
-    
+    public function setName(?string $name){
+        $this->name = $name;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function setEmail(?string $email){
+        $this->email = $email;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function setPassword(?string $password){
+        $this->password = $password;
+    }
+
+    public function getPassword(){
+        return $this->password;
+    }
+
+    public function setRoles(?array $roles){
+        $this->roles = $roles;
+    }
+
+    public function getRoles(){
+        return $this->roles;
+    }  
 }
