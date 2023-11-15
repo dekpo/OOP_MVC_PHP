ba3eacfef34184cc55a0c59dd8ee58bb7ada6e87
Dekpo Yologaza <dekpo@me.com>
2023-11-15 15:25:26 +0100

    Validate user's form

--- class/App/Controllers/UserController.php
+++ class/App/Controllers/UserController.php
@@ -6,15 +6,29 @@ use App\Models\User;
 class UserController extends Controller{
 
     public function index(){
+        // On instancie la class User pour créer un nouvel utilisateur
+        $user = new User();
+        // On anticipe d'éventuelles erreurs en créant un tableau
+        $errors = [];
         if (isset($_POST['submit'])){
-            $user = new User();
+            // Si le formulaire est validé on "hydrate" notre objet
+            // avec les informations du formulaire
             $user->setName($_POST['name']);
             $user->setEmail($_POST['email']);
             $user->setPassword($_POST['password']);
+            // Si la méthode validate ne retourne pas d'erreurs on fait l'insert dans la table
+            $errors = $user->validate();
+            if (empty($errors)){
+
+                echo "C'est gang ! On peut insérer l'utilisateur !";
+                die();
+
+            }
         }
         $this->render('./views/template_user.phtml',[
-            'user' => $user
+            '$_POST' => $_POST,
+            'user' => $user,
+            'errors' => $errors
         ]);
     }
-
 }
--- class/App/Models/User.php
+++ class/App/Models/User.php
@@ -39,5 +39,26 @@ class User extends AbstractTable{
 
     public function getRoles(){
         return $this->roles;
-    }  
+    }
+
+    public function validate() : array
+    {
+        $errors = [];
+        /**
+         * Pléthore de vérifications dans tous les sens
+         */
+        // Si le nom est inférieur à 3 caractères => error
+        if (strlen($this->name) < 3){
+            $errors[] = "Le champs nom est obligatoire, merci.";
+        }
+        // Si l'email n'est pas bien formé => error
+        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)){
+            $errors[] = "Veuillez renseigner un email valide s'il vous plait.";
+        }
+        // Si le mot de passe est inférieur à 3 caractères => error
+        if (strlen($this->password) < 3){
+            $errors[] = "Le mot-de-passe doit être au moins de 3 caractères, merci.";
+        }
+        return $errors;
+    }
 }
