96af87fc99343f9461bd33df0e2e1d1d35929c33
Dekpo Yolo <dekpo@me.com>
2023-12-20 17:23:28 +0100

    Added JWT login and auth to API controller

--- class/App/Controllers/ApiController.php
+++ class/App/Controllers/ApiController.php
@@ -2,6 +2,12 @@
 namespace App\Controllers;
 use App\Controllers\Controller;
 use App\Models\PictureManager;
+use Firebase\JWT\JWT;
+
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\SignatureInvalidException;
+use Firebase\JWT\BeforeValidException;
+use Firebase\JWT\Key;
 
 class ApiController extends Controller
 {
@@ -18,6 +24,37 @@ class ApiController extends Controller
        echo json_encode($_POST);
     }
 
+    public function login(){
+        $secret_key = "laclesecretedukgbciafbiettoutletoutime";
+        $user_id = 6; // This must be the user id retrieved by a query
 
+        $issued_at = time();
+        $expiration_time = $issued_at + (10 * 60); // valid for 10 minutes
+
+    $payload = array(
+        'iat' => $issued_at,
+        'exp' => $expiration_time,
+        'sub' => $user_id
+    );
+
+    echo JWT::encode($payload, $secret_key, 'HS256');
+    }
+
+    public function auth(){
+        $jwt_token = $_POST['jwt'];
+        $secret_key = "laclesecretedukgbciafbiettoutletoutime";
+        try {
+            return JWT::decode($jwt_token, new Key($secret_key, 'HS256'));
+        } catch (ExpiredException $e) {
+            throw new \Exception('Token expired');
+        } catch (SignatureInvalidException $e) {
+            throw new \Exception('Invalid token signature');
+        } catch (BeforeValidException $e) {
+            throw new \Exception('Token not valid yet');
+        } catch (\Exception $e) {
+            throw new \Exception('Invalid token');
+        }
+        echo "You're authenticated !";
+    }
 
 }
